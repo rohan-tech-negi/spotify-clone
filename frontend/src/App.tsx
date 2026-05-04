c2db180b8cf120525f44a5376082114923c3a1ca
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-04 10:26:57 +0530

    setup thhe shadcn for the setting up of the buttons and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
-
+import { Button } from "@/components/ui/button"
 
 function App() {
- 
-
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
