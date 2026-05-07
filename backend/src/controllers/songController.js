c03d34309154c1c928e129f1680f6e2d9a6acb79
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-07 11:19:14 +0530

    created a new controller feature for the gettig featured songs

--- backend/src/controllers/songController.js
+++ backend/src/controllers/songController.js
@@ -10,3 +10,27 @@ export const getAllSongs = async (req, res, next) => {
 		next(error);
 	}
 };
+
+export const getFeaturedSongs = async (req, res, next) => {
+	try {
+		// fetch 6 random songs using mongodb's aggregation pipeline
+		const songs = await Song.aggregate([
+			{
+				$sample: { size: 6 },
+			},
+			{
+				$project: {
+					_id: 1,
+					title: 1,
+					artist: 1,
+					imageUrl: 1,
+					audioUrl: 1,
+				},
+			},
+		]);
+
+		res.json(songs);
+	} catch (error) {
+		next(error);
+	}
+};
--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllSongs } from "../controllers/songController.js";
+import { getAllSongs , getFeaturedSongs} from "../controllers/songController.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 
 const router = Router();
 
-router.get("/", getAllSongs)
+router.get("/", protectRoute, requireAdmin, getAllSongs)
+router.get("/featured", getFeaturedSongs);
 
 export default router;
