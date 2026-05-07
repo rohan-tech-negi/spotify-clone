f31eb07c656331d7b3da4755d13a1c5f927f4342
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-07 11:04:12 +0530

    working with the deleting album endpoint

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -10,6 +10,7 @@ router.post("/songs", protectRoute, requireAdmin, createSong)
 router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong)
 
 router.post("/albums", protectRoute, requireAdmin, createAlbum)
+router.delete("/albums/:id", protectRoute, requireAdmin, deleteAlbum);
 
 export default router;
 
