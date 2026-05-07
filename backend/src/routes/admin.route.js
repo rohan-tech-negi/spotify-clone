3492210aa5e86b4d5d77c77edc1e7464c1c3ea61
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-07 11:09:30 +0530

    completed with all the routes of the admin routes

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -6,13 +6,15 @@ import { createSong , deleteSong, createAlbum, deleteAlbum, checkAdmin} from "..
 
 const router = Router();
 
-router.get("/check", protectRoute, requireAdmin, checkAdmin)
+router.use(protectRoute, requireAdmin)
 
-router.post("/songs", protectRoute, requireAdmin, createSong)
-router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong)
+router.get("/check", checkAdmin)
 
-router.post("/albums", protectRoute, requireAdmin, createAlbum)
-router.delete("/albums/:id", protectRoute, requireAdmin, deleteAlbum);
+router.post("/songs", createSong)
+router.delete("/songs/:id", deleteSong)
+
+router.post("/albums", createAlbum)
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
 
