bc12a8d712d1d09df650deebc3c02be6842052dd
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-08 21:55:34 +0530

    working on a componenet for the auth provider

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,36 @@
+import { LayoutDashboardIcon } from 'lucide-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Topbar = () => {
+    const isAdmin = false
+
+  return (
+    <div className='flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 
+      backdrop-blur-md z-10
+    '>
+        <div className='flex gap-2 items-center'>
+				{/* <img src='/spotify.png' className='size-8' alt='Spotify logo' /> */}
+				Spotify
+			</div>
+            <div className='flex items-center gap-4'>
+				{isAdmin && (
+					<Link to={"/admin"} >
+						<LayoutDashboardIcon className='size-4  mr-2' />
+						Admin Dashboard
+					</Link>
+				)}
+
+				<SignedOut>
+					<SignInOAuthButtons />
+				</SignedOut>
+
+				<UserButton />
+			</div>
+        
+        
+    </div>
+  )
+}
+
+export default Topbar
