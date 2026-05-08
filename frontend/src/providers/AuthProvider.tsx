4292d1a5a55791cfac91e0fe5d78d13d280b73c1
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-08 21:41:47 +0530

    as we area making an authprovider

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import { axiosInstance } from '@/lib/axios';
+import { useAuth } from '@clerk/clerk-react';
+import React, { useEffect, useState } from 'react'
+
+const updateApiToken = (token: string | null) => {
+	if (token) axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+	else delete axiosInstance.defaults.headers.common["Authorization"];
+};
 
 const AuthProvider = () => {
+    const { getToken, userId } = useAuth();
+	const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        const initAuth = async () => {
+			try {
+				const token = await getToken();
+				updateApiToken(token);
+				if (token) {
+					await checkAdminStatus();
+					// init socket
+					if (userId) initSocket(userId);
+				}
+			} catch (error: any) {
+				updateApiToken(null);
+				console.log("Error in auth provider", error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+
+    })
   return (
     <div>AuthProvider</div>
   )
