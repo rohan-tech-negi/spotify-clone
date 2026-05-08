13e74b1af9d569c265d8368edf468fd82860e6cf
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-08 21:37:40 +0530

    setting up the frontend with the setting up of the axios.ts file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
 
-    <App />
     </ClerkProvider>
   </StrictMode>,
 )
