829ce61364c3dae376eb43a3536bf07a91396b07
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-08 21:28:22 +0530

    setup the routes for the stats

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,10 +1,11 @@
 
 import { Router } from "express";
-
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { requireAdmin } from "../middleware/admin.middleware.js";
+// import { getStats } from "../controllers/statController.js";
+import { getStats } from "../controllers/statController.js";
 
 const router = Router();
 
-router.get("/", async(req,res, next)=>{
-
-})
+router.get("/", protectRoute, requireAdmin, getStats);
 export default router;
