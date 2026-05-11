64eb8960aba08a348f3a49b4657a739bb76c960b
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-11 13:07:20 +0530

    worked on the ui and now be using the shadcn components for the good layout

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
 const MainLayout = () => {
+
   return (
-    <div>MainLayout</div>
+    <div className='h-screen bg-black text-white flex flex-col'>
+			<ResizablePanelGroup direction='horizontal' className='flex-1 flex h-full overflow-hidden p-2'>
+				<AudioPlayer />
+				{/* left sidebar */}
+				<ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
+					<LeftSidebar />
+				</ResizablePanel>
+
+				<ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
+
+				{/* Main content */}
+				<ResizablePanel defaultSize={isMobile ? 80 : 60}>
+					<Outlet />
+				</ResizablePanel>
+
+				{!isMobile && (
+					<>
+						<ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
+
+						{/* right sidebar */}
+						<ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
+							<FriendsActivity />
+						</ResizablePanel>
+					</>
+				)}
+			</ResizablePanelGroup>
+
+			<PlaybackControls />
+		</div>
   )
 }
 
