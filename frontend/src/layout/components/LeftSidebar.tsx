a07fbf71d20f00de65067d23737fb845a620391e
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-11 13:15:06 +0530

    in the leftside bar working in different sections worked on the leftsidebar

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -2,7 +2,41 @@ import React from 'react'
 
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
+    <div className='h-full flex flex-col gap-2'>
+        {/* navigation menu */}
+        <div className='rounded-lg bg-zinc-900 p-4'>
+				<div className='space-y-2'>
+					<Link
+						to={"/"}
+						className={cn(
+							buttonVariants({
+								variant: "ghost",
+								className: "w-full justify-start text-white hover:bg-zinc-800",
+							})
+						)}
+					>
+						<HomeIcon className='mr-2 size-5' />
+						<span className='hidden md:inline'>Home</span>
+					</Link>
+
+					<SignedIn>
+						<Link
+							to={"/chat"}
+							className={cn(
+								buttonVariants({
+									variant: "ghost",
+									className: "w-full justify-start text-white hover:bg-zinc-800",
+								})
+							)}
+						>
+							<MessageCircle className='mr-2 size-5' />
+							<span className='hidden md:inline'>Messages</span>
+						</Link>
+					</SignedIn>
+				</div>
+			</div>
+
+    </div>
   )
 }
 
