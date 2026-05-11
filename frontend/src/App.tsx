b988f3d89b8a9625c560494f2ed9f246c7bceda0
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-11 13:06:12 +0530

    be working on the main layout page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,20 @@ import { AuthenticateWithRedirectCallback, SignedIn, SignedOut, SignInButton, Si
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/home/HomePage'
 import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage'
+import MainLayout from './layout/MainLayout'
 
 function App() {
   
   return (
    <>
     <Routes>
-      <Route path='/' element={<HomePage />} />
+      
       <Route path='/sso-callback' element={<AuthenticateWithRedirectCallback  signInForceRedirectUrl={"/auth-callback"}/>} />
       <Route path='/auth-callback' element={<AuthCallbackPage />} />
+
+      <Route  element={<MainLayout />} >
+          <Route path='/' element={<HomePage />} />
+      </Route>
       
     </Routes>
    </>
