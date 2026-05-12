5ab7e31bc85fb08c34c60d10489b3a479de52cce
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-12 15:16:56 +0530

    started our todays work with the library section of the leftsidebar

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -1,7 +1,7 @@
 import { buttonVariants } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { SignedIn } from '@clerk/clerk-react'
-import { HomeIcon, MessageCircle } from 'lucide-react'
+import { HomeIcon, Library, MessageCircle } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -40,7 +40,41 @@ const LeftSidebar = () => {
 					</SignedIn>
 				</div>
 			</div>
+							<div className='flex-1 rounded-lg bg-zinc-900 p-4'>
+				<div className='flex items-center justify-between mb-4'>
+					<div className='flex items-center text-white px-2'>
+						<Library className='size-5 mr-2' />
+						<span className='hidden md:inline'>Playlists</span>
+					</div>
+				</div>
 
+				<ScrollArea className='h-[calc(100vh-300px)]'>
+					<div className='space-y-2'>
+						{isLoading ? (
+							<PlaylistSkeleton />
+						) : (
+							albums.map((album) => (
+								<Link
+									to={`/albums/${album._id}`}
+									key={album._id}
+									className='p-2 hover:bg-zinc-800 rounded-md flex items-center gap-3 group cursor-pointer'
+								>
+									<img
+										src={album.imageUrl}
+										alt='Playlist img'
+										className='size-12 rounded-md flex-shrink-0 object-cover'
+									/>
+
+									<div className='flex-1 min-w-0 hidden md:block'>
+										<p className='font-medium truncate'>{album.title}</p>
+										<p className='text-sm text-zinc-400 truncate'>Album • {album.artist}</p>
+									</div>
+								</Link>
+							))
+						)}
+					</div>
+				</ScrollArea>
+			</div>
     </div>
   )
 }
