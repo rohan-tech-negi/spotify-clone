542d85b9c0f011071499f4e4cea1ecd5da311658
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-14 00:26:06 +0530

    working on the homepage.tsx

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,7 +1,26 @@
 import Topbar from '@/components/Topbar'
-import React from 'react'
+import { useMusicStore } from '@/stores/useMusicStore';
+import React, { useEffect } from 'react'
 
 const HomePage = () => {
+
+  const {
+		fetchFeaturedSongs,
+		fetchMadeForYouSongs,
+		fetchTrendingSongs,
+		isLoading,
+		madeForYouSongs,
+		featuredSongs,
+		trendingSongs,
+	} = useMusicStore();
+
+  	useEffect(() => {
+		fetchFeaturedSongs();
+		fetchMadeForYouSongs();
+		fetchTrendingSongs();
+	}, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
+  
   return (
     <main className="rounded-md overflow-hidden h-full bg-zinc-900">
       <Topbar />
