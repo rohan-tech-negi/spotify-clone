9ace17ad4ace9498a64cfbae71d5b546d2ccaef0
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-14 00:38:36 +0530

    working on the section grid now

--- frontend/src/pages/home/components/SectionGrid.tsx
+++ frontend/src/pages/home/components/SectionGrid.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+
+type SectionGridProps = {
+	title: string;
+	songs: Song[];
+	isLoading: boolean;
+};
+
 const SectionGrid = () => {
   return (
     <div>SectionGrid</div>
