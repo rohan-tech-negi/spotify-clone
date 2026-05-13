6b8720382995822214653b612e9a74cdd08a8a21
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-14 00:08:41 +0530

    working on a new other stores

---
+++ frontend/src/stores/useAuthStore.ts
@@ -0,0 +1,11 @@
+import { axiosInstance } from "@/lib/axios";
+import { create } from "zustand";
+
+interface AuthStore {
+	isAdmin: boolean;
+	isLoading: boolean;
+	error: string | null;
+
+	checkAdminStatus: () => Promise<void>;
+	reset: () => void;
+}
