7747cd802b898fb254ebc13cceb90dd58ba42564
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-13 11:03:41 +0530

    working on some types in the frontend

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Song {
+	_id: string;
+	title: string;
+	artist: string;
+	albumId: string | null;
+	imageUrl: string;
+	audioUrl: string;
+	duration: number;
+	createdAt: string;
+	updatedAt: string;
+}
