f0fdbc057a75b2263693cbd1e0a6d3b1bb00fa2f
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-15 01:05:50 +0530

    doing various functionality in the audioplayer

--- frontend/src/layout/components/AudioPlayer.tsx
+++ frontend/src/layout/components/AudioPlayer.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { usePlayerStore } from '@/stores/usePlayerStore';
+import React, { useEffect, useRef } from 'react'
 
 const AudioPlayer = () => {
   const audioRef = useRef<HTMLAudioElement>(null);
+  	const prevSongRef = useRef<string | null>(null);
+
+    const { currentSong, isPlaying, playNext } = usePlayerStore();
+
+    // handle play/pause logic
+	useEffect(() => {
+		if (isPlaying) audioRef.current?.play();
+		else audioRef.current?.pause();
+	}, [isPlaying]);
+
+  	useEffect(() => {
+		const audio = audioRef.current;
+
+		const handleEnded = () => {
+			playNext();
+		};
+
+		audio?.addEventListener("ended", handleEnded);
+
+		return () => audio?.removeEventListener("ended", handleEnded);
+	}, [playNext]);
+
+  
   return (
     <audio/>
   )
