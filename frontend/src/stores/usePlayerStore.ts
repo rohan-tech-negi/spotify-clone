23896a7da44c993ba76b19dbdf4bd5b2d97545be
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-15 00:53:24 +0530

    starting todays work with useplaystore

---
+++ frontend/src/stores/usePlayerStore.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+import { Song } from "@/types";
+
+interface PlayerStore {
+	currentSong: Song | null;
+	isPlaying: boolean;
+	queue: Song[];
+	currentIndex: number;
+
+	initializeQueue: (songs: Song[]) => void;
+	playAlbum: (songs: Song[], startIndex?: number) => void;
+	setCurrentSong: (song: Song | null) => void;
+	togglePlay: () => void;
+	playNext: () => void;
+	playPrevious: () => void;
+}
