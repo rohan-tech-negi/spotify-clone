311076dfd9194d94779281858f010a2be8b3a1dc
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-15 01:30:38 +0530

    working on the playbutton on the home page albums

--- frontend/src/pages/home/components/FeaturedSection.tsx
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -1,6 +1,7 @@
 import FeaturedGridSkeleton from '@/components/skeletons/FeaturedGridSkeleton';
 import { useMusicStore } from '@/stores/useMusicStore';
 import React from 'react'
+import PlayButton from './PlayButton';
 
 const FeaturedSection = () => {
 
@@ -28,7 +29,7 @@ const FeaturedSection = () => {
 						<p className='font-medium truncate'>{song.title}</p>
 						<p className='text-sm text-zinc-400 truncate'>{song.artist}</p>
 					</div>
-					{/* <PlayButton song={song} /> */}
+					<PlayButton song={song} />
           
 				</div>
 			))}
