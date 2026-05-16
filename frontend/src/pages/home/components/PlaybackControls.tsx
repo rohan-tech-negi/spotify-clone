da7c549a349d93359096b070d9df0f84410a6044
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-16 11:14:46 +0530

    defining various states for the data setting up

--- frontend/src/pages/home/components/PlaybackControls.tsx
+++ frontend/src/pages/home/components/PlaybackControls.tsx
@@ -1,9 +1,15 @@
+import { usePlayerStore } from '@/stores/usePlayerStore';
 import React from 'react'
 
 const PlaybackControls = () => {
+    const { currentSong, isPlaying, togglePlay, playNext, playPrevious } = usePlayerStore();
+
+    const [volume, setVolume] = useState(75);
+	const [currentTime, setCurrentTime] = useState(0);
+	const [duration, setDuration] = useState(0);
   return (
     <div>PlaybackControls</div>
   )
 }
 
-export default  
+export default PlaybackControls
