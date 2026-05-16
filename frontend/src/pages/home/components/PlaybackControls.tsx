fff83ffac0b4d0ffa02d9bdd9b6fc8a2d9141b6c
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-16 11:15:40 +0530

    digining the functionality of the audio playing in the useeffect

--- frontend/src/pages/home/components/PlaybackControls.tsx
+++ frontend/src/pages/home/components/PlaybackControls.tsx
@@ -1,5 +1,5 @@
 import { usePlayerStore } from '@/stores/usePlayerStore';
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const PlaybackControls = () => {
     const { currentSong, isPlaying, togglePlay, playNext, playPrevious } = usePlayerStore();
@@ -7,6 +7,34 @@ const PlaybackControls = () => {
     const [volume, setVolume] = useState(75);
 	const [currentTime, setCurrentTime] = useState(0);
 	const [duration, setDuration] = useState(0);
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+
+    useEffect(() => {
+		audioRef.current = document.querySelector("audio");
+
+		const audio = audioRef.current;
+		if (!audio) return;
+
+		const updateTime = () => setCurrentTime(audio.currentTime);
+		const updateDuration = () => setDuration(audio.duration);
+
+		audio.addEventListener("timeupdate", updateTime);
+		audio.addEventListener("loadedmetadata", updateDuration);
+
+		const handleEnded = () => {
+			usePlayerStore.setState({ isPlaying: false });
+		};
+
+		audio.addEventListener("ended", handleEnded);
+
+		return () => {
+			audio.removeEventListener("timeupdate", updateTime);
+			audio.removeEventListener("loadedmetadata", updateDuration);
+			audio.removeEventListener("ended", handleEnded);
+		};
+	}, [currentSong]);
+
+    
   return (
     <div>PlaybackControls</div>
   )
