ffe92dba8d0d97f9fbdb1360a2b6eb7de4449f28
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-16 11:24:00 +0530

    done with some queue work on the homepage

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from 'react'
 import FeaturedSection from './components/FeaturedSection';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import SectionGrid from './components/SectionGrid';
+import { usePlayerStore } from '@/stores/usePlayerStore';
 
 const HomePage = () => {
 
@@ -16,6 +17,15 @@ const HomePage = () => {
 		featuredSongs,
 		trendingSongs,
 	} = useMusicStore();
+
+	const {initializeQueue} = usePlayerStore()
+
+	useEffect(() => {
+		if (madeForYouSongs.length > 0 && featuredSongs.length > 0 && trendingSongs.length > 0) {
+			const allSongs = [...featuredSongs, ...madeForYouSongs, ...trendingSongs];
+			initializeQueue(allSongs);
+		}
+	}, [initializeQueue, madeForYouSongs, trendingSongs, featuredSongs]);
 
   	useEffect(() => {
 		fetchFeaturedSongs();
