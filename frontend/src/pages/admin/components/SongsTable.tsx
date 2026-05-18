fc9667a6631532a6231991fecb728a318094593e
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-18 22:57:53 +0530

    done with the songtable component

--- frontend/src/pages/admin/components/SongsTable.tsx
+++ frontend/src/pages/admin/components/SongsTable.tsx
@@ -1,3 +1,7 @@
+import { Button } from '@/components/ui/button';
+import { TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { useMusicStore } from '@/stores/useMusicStore';
+import { Calendar, Table, Trash2 } from 'lucide-react';
 import React from 'react'
 
 const SongsTable = () => {
@@ -19,7 +23,48 @@ const SongsTable = () => {
 		);
 	}
   return (
-    <div>SongsTable</div>
+    <Table>
+			<TableHeader>
+				<TableRow className='hover:bg-zinc-800/50'>
+					<TableHead className='w-[50px]'></TableHead>
+					<TableHead>Title</TableHead>
+					<TableHead>Artist</TableHead>
+					<TableHead>Release Date</TableHead>
+					<TableHead className='text-right'>Actions</TableHead>
+				</TableRow>
+			</TableHeader>
+
+			<TableBody>
+				{songs.map((song) => (
+					<TableRow key={song._id} className='hover:bg-zinc-800/50'>
+						<TableCell>
+							<img src={song.imageUrl} alt={song.title} className='size-10 rounded object-cover' />
+						</TableCell>
+						<TableCell className='font-medium'>{song.title}</TableCell>
+						<TableCell>{song.artist}</TableCell>
+						<TableCell>
+							<span className='inline-flex items-center gap-1 text-zinc-400'>
+								<Calendar className='h-4 w-4' />
+								{song.createdAt.split("T")[0]}
+							</span>
+						</TableCell>
+
+						<TableCell className='text-right'>
+							<div className='flex gap-2 justify-end'>
+								<Button
+									variant={"ghost"}
+									size={"sm"}
+									className='text-red-400 hover:text-red-300 hover:bg-red-400/10'
+									onClick={() => deleteSong(song._id)}
+								>
+									<Trash2 className='size-4' />
+								</Button>
+							</div>
+						</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
   )
 }
 
