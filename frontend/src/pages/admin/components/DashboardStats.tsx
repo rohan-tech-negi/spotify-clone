11c169de14c8346c71c2ca8219e31295b92e1702
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-18 22:49:57 +0530

    working on the dashboard stats page

--- frontend/src/pages/admin/components/DashboardStats.tsx
+++ frontend/src/pages/admin/components/DashboardStats.tsx
@@ -1,6 +1,9 @@
+import { useMusicStore } from '@/stores/useMusicStore';
 import React from 'react'
 
 const DashboardStats = () => {
+
+  const { stats } = useMusicStore();
   return (
     <div>DashboardStats</div>
   )
