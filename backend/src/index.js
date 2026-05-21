656dd921e7b717c6cd43d80b67ff6b3767b12a6a
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-21 22:17:32 +0530

    tried appling some changes in the code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
+import "dotenv/config";
 import express from "express";
-import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
@@ -18,7 +18,7 @@ import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
 
-dotenv.config();
+
 
 const __dirname = path.resolve();
 const app = express();
