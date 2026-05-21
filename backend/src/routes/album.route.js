6f7dd236e7613ae44ec97373b2d56be2db5d1a7a
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-21 22:00:43 +0530

    removed the old routes and adding new routes

--- backend/src/routes/album.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllAlbums, getAlbumById } from "../controllers/albumController.js";
-
-
-const router = Router();
-
-router.get("/", getAllAlbums)
-router.get("/:albumId", getAlbumById)
-export default router;
--- backend/src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { authCallback } from "../controllers/authController.js";
-
-
-const router = Router();
-
-router.post("/callback", authCallback)
-
-export default router;
